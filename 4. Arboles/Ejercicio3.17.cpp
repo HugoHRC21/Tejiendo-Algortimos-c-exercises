a2c733201459931359800572f91e6102c269b739
Hugo Rivas <159493617+HugoHRC21@users.noreply.github.com>
2025-11-21 09:24:07 -0400

    Add files via upload

---
+++ 4. Arboles/Ejercicio3.17.cpp
@@ -0,0 +1,4 @@
+// 25. Considere los siguientes recorridos sobre una arborescencia:
+// Prefijo : 4 2 1 3 18 16 12 8 5 6 7 9 10 11 14 13 15 17 19 20
+// Sufijo : 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+// Dibuje la arborescencia
